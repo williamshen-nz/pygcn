875ce55858f12db5634e449c6db909c1633c57f4
William Shen <william.w.y.shen@gmail.com>
2018-10-14 01:55:59 +1100

    Support multiple hidden layers

--- pygcn/models.py
+++ pygcn/models.py
@@ -4,15 +4,24 @@ from pygcn.layers import GraphConvolution
 
 
 class GCN(nn.Module):
-    def __init__(self, nfeat, nhid, nclass, dropout):
+    def __init__(self, nfeat, nhid, nclass, dropout, nlayers=1):
         super(GCN, self).__init__()
 
-        self.gc1 = GraphConvolution(nfeat, nhid)
-        self.gc2 = GraphConvolution(nhid, nclass)
+        self.input_layer = GraphConvolution(nfeat, nhid)
+        self.hidden_layers = []
+        for _ in range(nlayers - 1):
+            self.hidden_layers.append(GraphConvolution(nhid, nhid))
+        self.output_layer = GraphConvolution(nhid, nclass)
         self.dropout = dropout
 
     def forward(self, x, adj):
-        x = F.relu(self.gc1(x, adj))
+        # Pass input to input layer and apply dropout
+        x = F.relu(self.input_layer(x, adj))
         x = F.dropout(x, self.dropout, training=self.training)
-        x = self.gc2(x, adj)
+        # Pass hidden representations to each hidden layer and apply dropout
+        for hidden_layer in self.hidden_layers:
+            x = F.relu(hidden_layer(x, adj))
+            x = F.dropout(x, self.dropout, training=self.training)
+        # Pass hidden outputs to final output layer
+        x = self.output_layer(x, adj)
         return F.log_softmax(x, dim=1)
